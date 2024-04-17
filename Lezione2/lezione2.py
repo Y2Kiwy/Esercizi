3d10547299a1d95966ec4e01913b45b29a5d20be
Y2Kiwy <simoncello.anto@gmail.com>
2024-04-17 13:50:44 +0200

    Added "lezione2.py"

---
+++ Lezione2/lezione2.py
@@ -0,0 +1,49 @@
+# Simone Antonelli
+# 17/04/2024
+
+print("Hello World!")
+
+print("\n") # Formatting
+
+
+
+# Exercise 2-3 ------------------------------------------------------------------------------------------------------------------
+print("Excercise: 2-3. Personal Message ↓\n")
+
+name: str = "Gaia"
+message: str = f"\tHello {name}, would you like to learn some Python today?"
+
+print(message)
+
+print("\n") # Formatting
+# -------------------------------------------------------------------------------------------------------------------------------
+
+
+
+# Exercise 2-4 ------------------------------------------------------------------------------------------------------------------
+print("Excercise: 2-4. Name Cases ↓\n")
+
+name: str = "gAiA"
+
+lower_name: str = name.lower()
+upper_name: str = name.upper()
+title_name: str = name.title()
+
+#print(f"\tNormal name: {name}.\n\tLowercase name: {lower_name}.\n\tUppercase name: {upper_name}.\n\tTitle case name: {title_name}.")
+
+print(f"\tNormal name: {name}.")
+print(f"\tLowercase name: {lower_name}.")
+print(f"\tUppercase name: {upper_name}.")
+print(f"\tTitle case name: {upper_name}.")
+
+print("\n") # Formatting
+# -------------------------------------------------------------------------------------------------------------------------------
+
+
+
+# Exercise 2-5 ------------------------------------------------------------------------------------------------------------------
+print("Excercise: 2-5. Famous Quote ↓\n")
+
+
+
+# -------------------------------------------------------------------------------------------------------------------------------
