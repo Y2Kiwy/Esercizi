74358230254e09e6fcbc33a121209b4020750acc
Y2Kiwy <simoncello.anto@gmail.com>
2024-04-22 11:28:19 +0200

    End of exercise of 'lezione3.py'

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -258,13 +258,16 @@ print("\n") # Formatting
 # Exercise 5.3 #1 ---------------------------------------------------------------------------------------------------------------
 print("Excercise: 5-3. Alien Colors #1 ↓\n")
 
+# First quest
 alien_color: str = "Green"
 if alien_color == "Green":
     print("\tGood shoot, you have earned 5 points!")
-
 else:
     print("\n\tTry again")
 
+
+
+# Second quest
 alien_color: str = "Black"
 if alien_color == "Green":
     print("\n\tGood shoot, you have earned 5 points!")
@@ -277,19 +280,16 @@ print("\n") # Formatting
 # Exercise 5.4 ------------------------------------------------------------------------------------------------------------------
 print("Excercise: 5-4. Alien Colors #2 ↓\n")
 
-alien_color: str = "Red"
-if alien_color == "Green":
-    print("\tGood shoot, you have earned 5 points!")
-
-else:
-    print("\tGreat shoot, you have earned 10 points!")
-
+# First quest
 alien_color: str = "Green"
 if alien_color == "Green":
     print("\n\tGood shoot, you have earned 5 points!")
 
-else:
-    print("\tGreat shoot, you have earned 10 points!")
+
+# Second quest
+alien_color: str = "Red"
+if alien_color == "Red":
+    print("\tGood shoot, you have earned 10 points!")
 
 print("\n") # Formatting
 # -------------------------------------------------------------------------------------------------------------------------------
@@ -299,35 +299,152 @@ print("\n") # Formatting
 # Exercise 5.5 ------------------------------------------------------------------------------------------------------------------
 print("Excercise: 5-5. Alien Colors #3 ↓\n")
 
+# First quest
 alien_color: str = "Green"
 if alien_color == "Green":
     print("\tGood shoot, you have earned 5 points!")
 
-elif alien_color == "Yellow":
-    print("\tGreat shoot, you have earned 10 points!")
 
-else:
-    print("\tGreat shoot, you have earned 15 points!")
 
+# Second quest
 alien_color: str = "Yellow"
-if alien_color == "Green":
-    print("\tGood shoot, you have earned 5 points!")
-
-elif alien_color == "Yellow":
+if alien_color == "Yellow":
     print("\tGreat shoot, you have earned 10 points!")
 
-else:
-    print("\tGreat shoot, you have earned 15 points!")
 
+
+# Third quest
 alien_color: str = "Red"
-if alien_color == "Green":
+if alien_color == "Red":
     print("\tGood shoot, you have earned 5 points!")
 
-elif alien_color == "Yellow":
-    print("\tGreat shoot, you have earned 10 points!")
+print("\n") # Formatting
+# -------------------------------------------------------------------------------------------------------------------------------
+
+
+
+# Exercise 5.6 -----------------------------------------------------------------------------------------------------------------
+print("Excercise: 5-6. Stages of Life ↓\n")
+
+person_age: int = 19
+
+if person_age < 2:
+    print(f"\tThat the person is a baby.")
+elif 2 <= person_age < 4:
+    print(f"\tThat the person is a toddler.")
+elif 4 <= person_age < 13:
+    print(f"\tThat the person is a kid.")
+elif 13 <= person_age < 20:
+    print(f"\tThat the person is a teenager.")
+elif 20 <= person_age < 65:
+    print(f"\tThat the person is an adult.")
+elif person_age > 65:
+    print(f"\tThat the person is an elder.")
+
+print("\n") # Formatting
+# -------------------------------------------------------------------------------------------------------------------------------
+
+
+
+# Exercise 5.7 ------------------------------------------------------------------------------------------------------------------
+print("Excercise: 5-7. Favorite Fruit ↓\n")
+
+favorite_fruits: list = ["Banana", "Cherry", "Strawberry"]
+
+if "Cherry" in favorite_fruits:
+    print(f"\tCherry is in your list!")
+
+if "Watermelon" in favorite_fruits:
+    print(f"\tWatermelon is in your list!")
+
+if "Strawberry" in favorite_fruits:
+    print(f"\tStrawberry is in your list!")
+
+if "Apple" in favorite_fruits:  
+    print(f"\tApple is in your list!")
+
+if "Banana" in favorite_fruits:
+    print(f"\tBanana is in your list!")
+
+print("\n") # Formatting
+# -------------------------------------------------------------------------------------------------------------------------------
+
+
+
+# Exercise 5.8 -----------------------------------------------------------------------------------------------------------------
+print("Excercise: 5-8. Hello Admin ↓\n")
+
+users: list = ["Gaia", "Simone", "Admin", "Alessio", "Luca"]
+
+for user in users:
+    if user == "Admin":
+        print(f"\tDo you want to see a status report?")
+    else:
+        print(f"\tHi {user}")
+
+print("\n") # Formatting
+# -------------------------------------------------------------------------------------------------------------------------------
+
+
+
+# Exercise 5.9 -----------------------------------------------------------------------------------------------------------------
+print("Excercise: 5-9. No Users ↓\n")
+
+users: list = []
+
+if not users:
+    print("\tWe need to find some users!")
 
 else:
-    print("\tGreat shoot, you have earned 15 points!")
+    for user in users:
+
+        if user == "Admin":
+            print(f"\tDo you want to see a status report?")
+        else:
+            print(f"\tHi {user}")
+
+print("\n") # Formatting
+# -------------------------------------------------------------------------------------------------------------------------------
+
+
+
+# Exercise 5.10 -----------------------------------------------------------------------------------------------------------------
+print("Excercise: 5-10. Checking Usernames ↓\n")
+
+current_users: list = ["Gaia", "Simone", "Admin", "Alessio", "Luca"]
+new_users: list = ["Gaia", "Luca", "Fabio"]
+
+current_users_lowercase: list = [user.lower() for user in current_users]
+new_users_lowercase: list = [user.lower() for user in new_users]
+
+for user in new_users_lowercase:
+
+    if user in current_users_lowercase:
+        print(f"\tUser: '{user}' already exist!")
+    else:
+        print(f"\tUser: '{user}' is aviable!")
+
+
+print("\n") # Formatting
+# -------------------------------------------------------------------------------------------------------------------------------
+
+
+
+# Exercise 5.11 -----------------------------------------------------------------------------------------------------------------
+print("Excercise: 5-11. Ordinal Numbers ↓\n")
+
+numbers: list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for number in numbers:
+    if number == 1:
+        print(f"\t{number}st")
+    elif number == 2:
+        print(f"\t{number}nd")
+    elif number == 3:
+        print(f"\t{number}rd")
+    else:
+        print(f"\t{number}th")
+
 
 print("\n") # Formatting
 # -------------------------------------------------------------------------------------------------------------------------------
