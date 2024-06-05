56dbb3b85b102595b0f11181c80028bf7086b2ba
Y2Kiwy <simoncello.anto@gmail.com>
2024-06-05 12:33:21 +0200

    Added 'lezione16.py'

--- Lezione16/lezione16.py
+++ Lezione16/lezione16.py
@@ -35,27 +35,3 @@ class Media:
     
     def review(self) -> None:
         print(f"Titolo del film: {self.title}\nVoto Medio: {self.get_avg()}\nGiudizio: {self.get_rate()}\nTERRIBILE: {self.rate_percentage(1)}\nBRUTTO: {self.rate_percentage(2)}\nNORMALE: {self.rate_percentage(3)}\nBELLO: {self.rate_percentage(4)}\nGRANDIOSO: {self.rate_percentage(5)}")
-
-
-
-import unittest
-import io
-from unittest.mock import patch
-
-class TestMedia(unittest.TestCase):
-    def test_media_review(self):
-        media = Media("The Shawshank Redemption")
-        media.add_review(4)
-        media.add_review(5)
-        media.add_review(5)
-        media.add_review(4)
-        media.add_review(5)
-        
-        expected_output = "Titolo del film: The Shawshank Redemption\nVoto Medio: 4.60\nGiudizio: GRANDIOSO\nTERRIBILE: 0.00%\nBRUTTO: 0.00%\nNORMALE: 0.00%\nBELLO: 40.00%\nGRANDIOSO: 60.00%"
-        
-        with patch('sys.stdout', new=io.StringIO()) as fake_out:
-            media.review()
-            self.assertEqual(fake_out.getvalue().strip(), expected_output.strip())
-
-if __name__ == "__main__":
-    unittest.main()
